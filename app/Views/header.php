5119a9320fa657323deb176ecaf2ad65d163e0fb
zamyr <zamyr4a22@gmail.com>
2021-10-31 22:36:17 -0300

    Se corrigen cosas del dashboard

--- app/Views/header.php
+++ app/Views/header.php
@@ -61,7 +61,7 @@
 	    	</ul>
 	  	</nav>
 	  	<aside class="main-sidebar sidebar-dark-primary elevation-4">
-		    <a href="index3.html" class="brand-link">
+		    <a href="<?php echo base_url(); ?>" class="brand-link">
 	      		<img src="<?php echo base_url(); ?>/imagenes/icono.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
 	      		<span class="brand-text font-weight-light">SISPRO APR</span>
 	    	</a>
